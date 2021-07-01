8437482b5463a2804e7a2a5319178a09b3cea5fc
nikhildwivedi7 <nikhilsteamid@gmail.com>
2021-07-01 13:59:36 +0530

    updated todos.js - not adding blank todos now.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,21 +1,27 @@
 // Check Off specific Todos by clicking
-$("ul").on("click", "li", function(){
-	$(this).toggleClass("completed");
+$("ul").on("click", "li", function () {
+  $(this).toggleClass("completed");
 });
 //Click on X to delete Todo
-$("ul").on("click", "span", function(event){
-	$(this).parent().fadeOut(500, function(){
-		$(this).remove();
-	});
-	//event.stopPropagation();
+$("ul").on("click", "span", function (event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
+      $(this).remove();
+    });
+  //event.stopPropagation();
 });
 
-$("input[type='text']").keypress(function(event){
-	if(event.which === 13){
-		//grabbong new todo text from input
-		var todoText = $(this).val();
-		$(this).val("");
-		//create a new li and add to ul
-		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
-	}
+$("input[type='text']").keypress(function (event) {
+  if (event.which === 13) {
+    //grabbong new todo text from input
+    var todoText = $(this).val();
+    $(this).val("");
+    //create a new li and add to ul
+    if (todoText.trim() !== "") {
+      $("ul").append(
+        "<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>"
+      );
+    }
+  }
 });
